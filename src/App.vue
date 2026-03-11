ebe72599d2aa6142bf342cfab5da0a812cc0806b
Margarita <557795@student.fontys.nl>
2026-03-11 15:12:33 +0100

    Initialize an empty Vue project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Home</h1>
+  <p>
+    Here will soon be my website. I am currently working on it, so stay tuned for updates!   
+  </p>
+</template>
+
+<style scoped></style>
