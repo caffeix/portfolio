8c5ec58c6c1f81ea38f12cc9d5fa9ce33ec28130
Margarita <557795@student.fontys.nl>
2026-03-18 13:36:33 +0100

    feat(about-me): add type declarations and data for navbar and profile section
    Declare types and add content for navbar items and profile information (bio)
    Add cv to be downloaded
    Add portrait picture

---
+++ src/data/profile.ts
@@ -0,0 +1,10 @@
+import type { Profile } from "@/types/profile";
+
+export const profile: Profile = {
+  name: "Margarita Fedulova",
+  title: "Frontend Engineer",
+  subtitle: "Fontys ICT | Delta Excellence Program",
+  description: "My passion is to build fast, modern-looking websites that create value. I am accustomed to working in multi-disciplinary projects and working my way up to finding solutions. I am also a novice electric guitar player and a passionate at-home cook. :)",
+  cvUrl: "/margarita_fedulova_cv.pdf",
+  portraitUrl: "/src/assets/Portrait.JPG",
+};
