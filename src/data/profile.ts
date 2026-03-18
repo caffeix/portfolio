852cd98a12b580738beb81701cae9ceb59b7683c
Margarita <557795@student.fontys.nl>
2026-03-18 14:47:14 +0100

    fix(about-me): fix portreit display
    - Fix the error where the portreit wouldn't get displayed in Vercel by manually declaring image extensions (.jpg, .png, etc.)

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,4 +1,5 @@
 import type { Profile } from "@/types/profile";
+import portraitUrl from "@/assets/portrait.jpg";
 
 export const profile: Profile = {
   name: "Margarita Fedulova",
@@ -6,5 +7,5 @@ export const profile: Profile = {
   subtitle: "Fontys ICT | Delta Excellence Program",
   description: "My passion is to build fast, modern-looking websites that create value. I am accustomed to working in multi-disciplinary projects and working my way up to finding solutions. I am also a novice electric guitar player and a passionate at-home cook. :)",
   cvUrl: "/margarita_fedulova_cv.pdf",
-  portraitUrl: "/src/assets/Portrait.JPG",
+  portraitUrl,
 };
